02498f2eabf19e5d8bf231d87415b0fa5927a3ad
Jonny <54864070+JonnySC@users.noreply.github.com>
2021-06-20 22:18:03 -0500

    Create spring.py

---
+++ spring.py
@@ -0,0 +1,7 @@
+import networkx as nx  # Importación del paquete NetworkX
+import matplotlib.pyplot as plt  # Importación paquete Matplotlib con el módulo pyplot
+
+G = nx.random_regular_graph(5, 100)  # Función generadora de un grafo d-regular random de n vértices
+nx.draw_spring(G, node_size=50, width=0.5, with_labels=False)  # Dibujar el grafo G con una interfaz particular
+plt.axis("equal")  # Redimensionar los ejes a longitudes iguales
+plt.show()  # Mostrar el grado d-regular por pantalla
